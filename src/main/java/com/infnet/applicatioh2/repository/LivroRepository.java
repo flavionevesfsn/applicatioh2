b3512e2e077bef38aaaa96109fafb5f1a4c23837
flavionevesfsn <flavioneves89@gmail.com>
2024-09-11 21:24:39 -0300

    Minha segunda atualização

--- src/main/java/com/infnet/applicatioh2/repository/LivroRepository.java
+++ src/main/java/com/infnet/applicatioh2/repository/LivroRepository.java
@@ -4,4 +4,5 @@ import com.infnet.applicatioh2.entity.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    // outro teste
 }
