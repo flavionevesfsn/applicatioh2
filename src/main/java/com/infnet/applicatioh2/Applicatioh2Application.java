5c410b451be41be1951139558c9650e03a3beda9
Abrahao02 <112214564+Abrahao02@users.noreply.github.com>
2025-11-27 21:45:42 -0300

    Criando conflito

--- src/main/java/com/infnet/applicatioh2/Applicatioh2Application.java
+++ src/main/java/com/infnet/applicatioh2/Applicatioh2Application.java
@@ -8,6 +8,7 @@ public class Applicatioh2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Applicatioh2Application.class, args);
+		System.out.println("Criando conflito, para resolvermos!");
 	}
 
 }
